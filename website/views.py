beb58c8fdd791e3431a6c2b32eb92fd02e527e7b
EdMir <canonnist@gmail.com>
2023-01-08 18:30:10 +0100

    add twitter API fucntionality

--- website/views.py
+++ website/views.py
@@ -1,11 +1,29 @@
 from flask import Blueprint, render_template, redirect, url_for
+from .env import twi_api_key, twi_api_secret, twi_access_token, twi_access_secret
+import tweepy
+
 
 views = Blueprint('views', __name__)
+
+# Authenticate with Twitter
+auth_twi = tweepy.OAuthHandler(twi_api_key, twi_api_secret)
+auth_twi.set_access_token(twi_access_token, twi_access_secret)
+twi_api = tweepy.API(auth_twi)
+
+#test connection to twitter API 
+try:
+    twi_api.verify_credentials()
+    print('Successful Authentication Twitter')
+except:
+    print('Failed Twitter authentication')
+
+
 
 @views.route('/')
 @views.route('/home')
 def home():
-    return render_template('home.html')
+    tweets = twi_api.user_timeline(count=3)
+    return render_template('home.html', tweets=tweets)
 
 @views.route('/about-me')
 def about_me():
