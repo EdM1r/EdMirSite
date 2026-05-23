cceba7fb866f4e0b805851a6f52bb0fd9942facd
EdMir <canonnist@gmail.com>
2026-05-23 17:52:55 +0200

    handling missing env.py by falling back to env

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,24 @@
 from flask import Flask
-from .env import app_secret_key
 import os 
+
+try:
+    from .env import app_secret_key
+except Exception:
+    app_secret_key = None
+
+
+def _get_secret_key():
+    return (
+        os.environ.get('APP_SECRET_KEY')
+        or os.environ.get('FLASK_SECRET_KEY')
+        or os.environ.get('SECRET_KEY')
+        or app_secret_key
+        or 'change-me-in-production'
+    )
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = app_secret_key
+    app.config['SECRET_KEY'] = _get_secret_key()
 
     from .views import views
     app.register_blueprint(views, url_prefix='/')
